c6785c5dfb731565ad4492c901c6cc384f9c9cf1
SithiX <sithijax2@gmail.com>
2025-08-15 22:13:11 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
 import { useState } from 'react'
 import './App.css'
+import { GoGraph } from "react-icons/go";
+import { FaRegBookmark } from "react-icons/fa";
+import { MdOutlineSpeaker } from "react-icons/md";
+import { MdOutlineRateReview } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className='w-full h-screen flex'>
+        <div className= 'w-[400px] h-full bg-gray-200'>
+          <button className='flex justify-center items-center w-full h-[40px]  text-[25px] font-bold'>
+            <GoGraph />
+             Dashboard
+          </button>
+
+          <button className='flex justify-center items-center w-full h-[40px] text-[25px] font-bold'>
+            <FaRegBookmark />
+            Bookings
+          </button>
+
+        <button className='flex justify-center items-center w-full h-[40px] text-[25px] font-bold'>
+            <MdOutlineSpeaker />
+            Items
+          </button>
+
+        <button className='flex justify-center items-center w-full h-[40px] text-[25px] font-bold'>
+            <MdOutlineRateReview />
+            Reviews
+          </button>
+
+        <button className='flex justify-center items-center w-full h-[40px] text-[25px] font-bold'>
+            <FaRegUser />
+            Users
+          </button>
+
+      </div>
+          <div className='w-full  bg-red-900'>
+            
+          </div>
+
+      </div>
     </>
   )
 }
